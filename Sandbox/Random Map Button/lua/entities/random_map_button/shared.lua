38b31be9b4eb84e173ae760596c8aee708e7a1d3
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2021-06-01 13:40:05 -0700

    Added random map button from sandbox

---
+++ Sandbox/Random Map Button/lua/entities/random_map_button/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Random Map Change Button"
+ENT.Author = "Lambda Gaming"
+ENT.Category = "OPG Customs"
+ENT.Spawnable = true
+ENT.AdminOnly = true
